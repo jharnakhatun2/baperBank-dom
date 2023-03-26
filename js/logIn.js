783856e1d6410d19565d04f0f8662b365f42e80b
Jharna203 <jharnakhatun203@gmail.com>
2023-03-26 22:38:49 +0600

    added welcome login form

---
+++ js/logIn.js
@@ -0,0 +1,11 @@
+document.getElementById("submit_btn").addEventListener("click",function(){
+    const emailField = document.getElementById("email");
+    const email = emailField.value;
+    const passwordField = document.getElementById("password");
+    const password = passwordField.value;
+    if(email === "test@gmail.com" && password === "secret"){
+        window.location.href = "bank.html";
+    }else{
+        alert("Invalid User");
+    }
+});
